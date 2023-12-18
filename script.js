4d271d007313b2e5eab1edddd71d5b5cd3b67311
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-18 11:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-function stringChop(str, size) {
+function stringChop(str, size) 
+{
   // your code here
+	let ans=[];
+	for(let i=0;i<str.length;)
+	{
+		let last=str.length;
+        if(last-i+1>=size){
+            let x=size;
+            let curr="";
+            while(x-->0){
+                curr+=str.charAt(i);
+                i++;
+            }
+            ans.push(curr);
+        }
+        else{
+            let x=size;
+            let curr="";
+            while(i<str.length){
+                curr+=str.charAt(i);
+                i++;
+            }
+            ans.push(curr);
+        }
+	}
+
+    console.log(ans);
+
+	return ans;
+	
 }
 
 // Do not change the code below
